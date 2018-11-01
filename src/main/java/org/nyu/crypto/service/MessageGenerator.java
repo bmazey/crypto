93f0b83e791a1432234dbe3ad20849bbbaa1d687
Brandon <b.mazey@nyu.edu>
2018-11-01 16:36:42 -0400

    adding some services and some TODOs

---
+++ src/main/java/org/nyu/crypto/service/MessageGenerator.java
@@ -0,0 +1,59 @@
+package org.nyu.crypto.service;
+
+
+import org.nyu.crypto.dto.Dictionary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+
+@Service
+public class MessageGenerator {
+
+    @Autowired
+    private DictionaryGenerator dictionaryGenerator;
+
+    private final int MESSAGE_SPACE = 500;
+
+    private Random r;
+
+    public String generateMessage() {
+        r = new Random();
+        StringBuilder messageBuilder = new StringBuilder();
+        Dictionary dictionary = dictionaryGenerator.generateDictionary();
+
+        while(messageBuilder.length() < 500) {
+            messageBuilder.append(dictionary.getWords()[r.nextInt(dictionary.getWords().length)]);
+            messageBuilder.append(" ");
+        }
+
+        return messageBuilder.subSequence(0, 500).toString();
+    }
+
+    public String generateSubsetMessage() {
+        r = new Random();
+        StringBuilder messageBuilder = new StringBuilder();
+        Dictionary dictionary = dictionaryGenerator.generateDictionary();
+
+        /**
+         * I really like the idea of having a method that can shorten the size of the dictionary as I believe there
+         * will be many testing scenarios where we will want to measure performance based on this.
+         *
+         * b.mazey@nyu.edu
+         */
+
+        // TODO - make this more customizable
+        // here we assert the first word comes from the first ten entries in the dictionary
+        messageBuilder.append(dictionary.getWords()[10]);
+        messageBuilder.append(" ");
+
+        while(messageBuilder.length() < 500) {
+            messageBuilder.append(dictionary.getWords()[r.nextInt(dictionary.getWords().length)]);
+            messageBuilder.append(" ");
+        }
+
+        return messageBuilder.subSequence(0, 500).toString();
+    }
+
+}
