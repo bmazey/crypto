20fb7570bebde44746b5330e374f53402a520188
nealb <sb6856@nyu.edu>
2018-11-05 16:31:33 -0500

    message controller write up. generates 500 length random messages using the dictionary

--- src/main/java/org/nyu/crypto/controller/MessageController.java
+++ src/main/java/org/nyu/crypto/controller/MessageController.java
@@ -1,6 +1,25 @@
 package org.nyu.crypto.controller;
 
+import org.nyu.crypto.service.MessageGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MessageController {
 
-    //TODO - make this controller!
+    /**
+     * Return a random 500 length plaintext message for encryption
+     */
+    @Autowired
+    MessageGenerator messageGenerator;
+
+    @RequestMapping(value="/api/message", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> getMessage() {
+        return ResponseEntity.ok(messageGenerator.generateMessage());
+    }
 }
