c8d30e35d7fdc805c7f0af282e2cbc460922e3e9
Brandon <b.mazey@nyu.edu>
2018-11-09 15:27:02 -0500

    hotfix!

--- src/main/java/org/nyu/crypto/controller/DictionaryController.java
+++ src/main/java/org/nyu/crypto/controller/DictionaryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-public class DicitionaryController {
+public class DictionaryController {
 
     @Autowired
     private DictionaryGenerator dictionaryGenerator;
