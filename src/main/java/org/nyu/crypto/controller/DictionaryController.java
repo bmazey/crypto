fce3b967174e5e5ffdaa1a8686badfa8f2d1115b
Yuvraj Rajeev Singh <yuvraj.singh19@gmail.com>
2018-11-11 12:12:37 -0500

    * Modified controller to accommodate regexp * TODO- Need to figure out a plan for custom error message

--- src/main/java/org/nyu/crypto/controller/DictionaryController.java
+++ src/main/java/org/nyu/crypto/controller/DictionaryController.java
@@ -4,7 +4,7 @@ import org.nyu.crypto.service.DictionaryGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.http.HttpStatus;
 
 @RestController
 public class DictionaryController {
@@ -18,11 +18,12 @@ public class DictionaryController {
         return ResponseEntity.ok(dictionaryGenerator.generateDictionaryDto());
     }
 
-    //TODO - need to validate
-    @RequestMapping(value="/api/dictionary/{size}", method = RequestMethod.GET)
+    //TODO- Accepts valid sizes now, but needs to throw custom error message
+    @RequestMapping(value="/api/dictionary/{size:^[1-9][0-9]?$|^100$}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<?> getSubsetDictionary(@PathVariable("size") int size) {
         return ResponseEntity.ok(dictionaryGenerator.generateDictionaryDto(size));
     }
+
 
 }
