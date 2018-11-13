3790eecbbf8794f50576863f557c886bdd705c39
Brandon <b.mazey@nyu.edu>
2018-11-13 11:33:38 -0500

    ok enabled a repository

--- src/main/java/org/nyu/crypto/CryptoApplication.java
+++ src/main/java/org/nyu/crypto/CryptoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-//@EnableJpaRepositories()
+@EnableJpaRepositories("org.nyu.crypto.service.data")
 public class CryptoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoApplication.class, args);
