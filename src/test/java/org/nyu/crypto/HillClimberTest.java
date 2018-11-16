f6e8cac5c11a77e83566c6458aecdfa5b2acb227
Brandon <b.mazey@nyu.edu>
2018-11-16 12:08:14 -0500

    trying something dumb

--- src/test/java/org/nyu/crypto/HillClimberTest.java
+++ src/test/java/org/nyu/crypto/HillClimberTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nyu.crypto.dto.Simulation;
+import org.nyu.crypto.service.Decryptor;
 import org.nyu.crypto.service.Simulator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +24,9 @@ public class HillClimberTest {
     @Autowired
     private Simulator simulator;
 
+    @Autowired
+    private Decryptor decryptor;
+
     private ObjectMapper mapper = new ObjectMapper();
 
     private Logger logger = LoggerFactory.getLogger(HillClimberTest.class);
@@ -40,6 +44,22 @@ public class HillClimberTest {
 
         logger.info(plaintext);
         logger.info(Arrays.toString(ciphertext));
+
+        // let's create a second simulation
+        Simulation simulationTwo = simulator.createSimulation();
+
+        // unpack the contents into key, plaintext, ciphertext
+        HashMap<String, ArrayList<Integer>> keyTwo = mapper.convertValue(simulationTwo.getKey(), HashMap.class);
+        String plaintextTwo = simulationTwo.getMessage();
+        int[] ciphertextTwo = simulationTwo.getCiphertext();
+
+        logger.info(plaintextTwo);
+        logger.info(Arrays.toString(ciphertextTwo));
+
+        // here we try something dumb ... decrypt with mismatched
+        String putative = decryptor.decrypt(key, ciphertextTwo);
+
+        logger.info(putative);
 
     }
 }
