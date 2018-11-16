36a83626fc9c45b4677a849b36faec2aef8a6d7f
Brandon <b.mazey@nyu.edu>
2018-11-16 13:15:10 -0500

    adjacency matrix done!

--- src/test/java/org/nyu/crypto/HillClimberTest.java
+++ src/test/java/org/nyu/crypto/HillClimberTest.java
@@ -5,16 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nyu.crypto.dto.Simulation;
 import org.nyu.crypto.service.Decryptor;
+import org.nyu.crypto.service.KeyGenerator;
 import org.nyu.crypto.service.Simulator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 
 @RunWith(SpringRunner.class)
@@ -26,6 +29,12 @@ public class HillClimberTest {
 
     @Autowired
     private Decryptor decryptor;
+
+    @Autowired
+    private KeyGenerator keyGenerator;
+
+    @Value("${key.space}")
+    private int keyspace;
 
     private ObjectMapper mapper = new ObjectMapper();
 
@@ -45,21 +54,31 @@ public class HillClimberTest {
         logger.info(plaintext);
         logger.info(Arrays.toString(ciphertext));
 
-        // let's create a second simulation
-        Simulation simulationTwo = simulator.createSimulation();
+        // let's invoke the hill climbing function
+        climbHill(ciphertext);
 
-        // unpack the contents into key, plaintext, ciphertext
-        HashMap<String, ArrayList<Integer>> keyTwo = mapper.convertValue(simulationTwo.getKey(), HashMap.class);
-        String plaintextTwo = simulationTwo.getMessage();
-        int[] ciphertextTwo = simulationTwo.getCiphertext();
 
-        logger.info(plaintextTwo);
-        logger.info(Arrays.toString(ciphertextTwo));
+    }
 
-        // here we try something dumb ... decrypt with mismatched
-        String putative = decryptor.decrypt(key, ciphertextTwo);
+    private void climbHill(int[] ciphertext) {
+        // generate a random key
+        HashMap<String, ArrayList<Integer>> key = keyGenerator.generateKey();
 
-        logger.info(putative);
+        int[][] encrypted = new int[keyspace][keyspace];
+        int[][] putative = new int[keyspace][keyspace];
 
+        int[][] result = calculateAdjacency(encrypted, ciphertext);
+
+        Stream.of(result).map(Arrays::toString).forEach(System.out::println);
+
+    }
+
+    // this method calculates the adjacency of sets of numbers
+    private int[][] calculateAdjacency(int[][] encrypted, int[] ciphertext) {
+        // we don't have to check the last value, so we stop at length - 1
+        for (int i = 0; i < ciphertext.length - 1; i++) {
+            encrypted[ciphertext[i]][ciphertext[i + 1]] = 1;
+        }
+        return encrypted;
     }
 }
