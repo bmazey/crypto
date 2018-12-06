849775affb37de47299da7a0f91d154a19449a65
Francois Van Laer <fvl209@nyu.edu>
2018-12-06 16:52:29 -0500

    Fixing things

--- src/main/java/org/nyu/crypto/service/KeyGenerator.java
+++ src/main/java/org/nyu/crypto/service/KeyGenerator.java
@@ -55,72 +55,67 @@ public class KeyGenerator {
 
         HashMap<String, Integer> map = frequencyGenerator.generateFrequency();
         HashMap<String, ArrayList<Integer>> putativeKey = new HashMap<>();
-        ArrayList<Integer> whitelist = new ArrayList<>();
-        ArrayList<Integer> blacklist = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>(IntStream.range(0, keyspace).boxed().collect(toSet()));
+        HashSet<Integer> whitelist = new HashSet<>();
+        HashSet<Integer> blacklist = new HashSet<>();
+        ArrayList<Integer> bValue = new ArrayList<>();
 
-        // possible spaces
+        // possible spaces and possible b
 
         blacklist.add(ciphertext[0]); //first character
         blacklist.add(ciphertext[1]); //second character
         blacklist.add(ciphertext[2]); //third character
         blacklist.add(ciphertext[104]); //last character
 
-        for(int i = 0; i < ciphertext.length; i++){
+        for(int i = 0; i < ciphertext.length - 1; i++){
             if (ciphertext[i] == ciphertext[i+1] && !blacklist.contains(ciphertext[i])){
                 blacklist.add(ciphertext[i]);
+                bValue.add(ciphertext[i]);
                 i++;
             }
             else if (whitelist.contains(ciphertext[i])){
-                if (!blacklist.contains(ciphertext[i+1])){
-                    blacklist.add(ciphertext[i+1]);
-                }
-                if (!blacklist.contains(ciphertext[i+2])){
-                    blacklist.add(ciphertext[i+2]);
-                }
-                if (!blacklist.contains(ciphertext[i+3])){
-                    blacklist.add(ciphertext[i+3]);
-                }
+                if (i < ciphertext.length - 1)
+                        blacklist.add(ciphertext[i + 1]);
+                if (i < ciphertext.length - 2)
+                        blacklist.add(ciphertext[i + 2]);
+                if (i < ciphertext.length - 3)
+                        blacklist.add(ciphertext[i + 3]);
             }
             else
                 whitelist.add(ciphertext[i]);
         }
 
-        Collections.shuffle(whitelist);
-        ArrayList<Integer> spaceValues = new ArrayList<>(whitelist.subList(0, 19));
-        blacklist.addAll(whitelist.subList(19, whitelist.size() + 1));
+        ArrayList<Integer> spaceValues = new ArrayList<>(whitelist);
 
         putativeKey.put("space", spaceValues);
 
-        // possible b
-
-        int counter = 0;
-        ArrayList<Integer> bValue = new ArrayList<>();
-
-        for (int i = 0; i < ciphertext.length; i++){
-            if (ciphertext[i] == ciphertext[i+1] && blacklist.contains(ciphertext[i]) && counter == 0){
-                bValue.add(ciphertext[i]);
-                blacklist.remove(ciphertext[i]);
-                counter++;
-            }
+        if (bValue.size() > 1){
+            Random random = new Random();
+            int b = bValue.get(random.nextInt(bValue.size()));
+            bValue.clear();
+            bValue.add(b);
         }
+
         putativeKey.put("b", bValue);
 
-        Collections.shuffle(blacklist);
-        int partition = 0;
+        Collections.shuffle(numbers);
 
-        for(String key: map.keySet()) {
-            if (key.equals("space")){
-                partition = partition + map.get(key);
-            }
-            else if (key.equals("b")){
-                partition = partition + map.get(key);
-            }
-            else {
-                putativeKey.put(key, new ArrayList<>(blacklist.subList(partition, partition + map.get(key))));
-                partition = partition + map.get(key);
-            }
-        }
-        return putativeKey;
+//        Collections.shuffle(blacklist);
+//        int partition = 0;
+//
+//        for(String key: map.keySet()) {
+//            if (key.equals("space")){
+//                partition = partition + map.get(key);
+//            }
+//            else if (key.equals("b")){
+//                partition = partition + map.get(key);
+//            }
+//            else {
+//                putativeKey.put(key, new ArrayList<>(blacklist.subList(partition, partition + map.get(key))));
+//                partition = partition + map.get(key);
+//            }
+//        }
+//        return putativeKey;
     }
 
 }
