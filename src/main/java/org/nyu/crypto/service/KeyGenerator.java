94ee84b7039f8cc3d3ca78859de2a768cf3e4cdc
Francois Van Laer <fvl209@nyu.edu>
2018-12-06 15:33:36 -0500

    Working on an optimal key generator (space and b added)

--- src/main/java/org/nyu/crypto/service/KeyGenerator.java
+++ src/main/java/org/nyu/crypto/service/KeyGenerator.java
@@ -51,9 +51,61 @@ public class KeyGenerator {
         }
         return result;
     }
-
-    // TODO - use this to optimize our initial putative key guess?
     public HashMap<String, ArrayList<Integer>> generatePutativeKey(int[] ciphertext) {
+
+        HashMap<String, Integer> map = frequencyGenerator.generateFrequency();
+        HashMap<String, ArrayList<Integer>> putativeKey = new HashMap<>();
+        ArrayList<Integer> whitelist = new ArrayList<>();
+        ArrayList<Integer> blacklist = new ArrayList<>();
+
+        // possible spaces
+
+        blacklist.add(ciphertext[0]); //first character
+        blacklist.add(ciphertext[1]); //second character
+        blacklist.add(ciphertext[2]); //third character
+        blacklist.add(ciphertext[104]); //last character
+
+        for(int i = 0; i < ciphertext.length; i++){
+            if (ciphertext[i] == ciphertext[i+1] && !blacklist.contains(ciphertext[i])){
+                blacklist.add(ciphertext[i]);
+                i++;
+            }
+            else if (whitelist.contains(ciphertext[i])){
+                if (!blacklist.contains(ciphertext[i+1])){
+                    blacklist.add(ciphertext[i+1]);
+                }
+                if (!blacklist.contains(ciphertext[i+2])){
+                    blacklist.add(ciphertext[i+2]);
+                }
+                if (!blacklist.contains(ciphertext[i+3])){
+                    blacklist.add(ciphertext[i+3]);
+                }
+            }
+            else
+                whitelist.add(ciphertext[i]);
+        }
+
+        Collections.shuffle(whitelist);
+        ArrayList<Integer> spaceValues = new ArrayList<>(whitelist.subList(0, 19));
+        blacklist.addAll(whitelist.subList(19, whitelist.size() + 1));
+
+        putativeKey.put("space", spaceValues);
+
+        // possible b
+
+        int counter = 0;
+        ArrayList<Integer> bValue = new ArrayList<>();
+
+        for (int i = 0; i < ciphertext.length; i++){
+            if (ciphertext[i] == ciphertext[i+1] && blacklist.contains(ciphertext[i]) && counter == 0){
+                bValue.add(ciphertext[i]);
+                blacklist.remove(ciphertext[i]);
+                counter++;
+            }
+        }
+        putativeKey.put("b", bValue);
+
+
         return new HashMap<>();
     }
 
