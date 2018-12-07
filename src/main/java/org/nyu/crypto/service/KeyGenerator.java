1390c0a4a192cb3acd139684364dd8678754affc
Francois Van Laer <fvl209@nyu.edu>
2018-12-07 13:02:19 -0500

    Progress ?

--- src/main/java/org/nyu/crypto/service/KeyGenerator.java
+++ src/main/java/org/nyu/crypto/service/KeyGenerator.java
@@ -65,91 +65,153 @@ public class KeyGenerator {
         blacklist.add(ciphertext[0]); //first character
         blacklist.add(ciphertext[1]); //second character
         blacklist.add(ciphertext[2]); //third character
-        blacklist.add(ciphertext[105]); //last character
 
-        for(int i = 0; i < ciphertext.length - 1; i++){
+        for (int i = 0; i<ciphertext.length; i++){
+            if(!blacklist.contains(ciphertext[i])){
+                whitelist.add(ciphertext[i]);
+            }
+        }
 
-            if (ciphertext[i] == ciphertext[i+1]) {
+        for(int i = 0; i < ciphertext.length - 1; i++) {
+            if (ciphertext[i] == ciphertext[i + 1]) {
                 bValue.add(ciphertext[i]);
-                blacklist.remove(ciphertext[i]);
+                blacklist.add(ciphertext[i]);
                 whitelist.remove(ciphertext[i]);
             }
+        }
 
-            else if (whitelist.contains(ciphertext[i]) && !bValue.contains(ciphertext[i])){
-                if (i < ciphertext.length - 1) {
-                    blacklist.add(ciphertext[i + 1]);
-                    whitelist.remove(ciphertext[i + 1]);
+        ArrayList<Integer> possibleSpaceValues = new ArrayList<>(whitelist);
+        ArrayList<Integer> spaceValues = new ArrayList<>();
+
+        int space = 0;
+        while(space != 19) {
+            Collections.shuffle(possibleSpaceValues);
+            spaceValues = new ArrayList<>(possibleSpaceValues.subList(0, 19));
+            space = 0;
+            for (int i = 0; i < ciphertext.length - 3; i ++){
+                if (spaceValues.contains(ciphertext[i])){
+                    if (spaceValues.contains(ciphertext[i+1]) ||
+                            spaceValues.contains(ciphertext[i+2]) ||
+                            spaceValues.contains(ciphertext[i+3])){
+                        break;
+                    }
+                    else
+                        space++;
+
                 }
-                if (i < ciphertext.length - 2) {
-                    blacklist.add(ciphertext[i + 2]);
-                    whitelist.remove(ciphertext[i + 2]);
-                }
-                if (i < ciphertext.length - 3) {
-                    blacklist.add(ciphertext[i + 3]);
-                    whitelist.remove(ciphertext[i + 3]);
-                }
-            }
-            else if (!blacklist.contains(ciphertext[i]) && !bValue.contains(ciphertext[i]))
-                whitelist.add(ciphertext[i]);
-        }
-
-        if (bValue.size() > 1){
-            ArrayList<Integer> bTemp = new ArrayList<>(bValue);
-            Collections.shuffle(bTemp);
-            bValue.clear();
-            bValue.addAll(bTemp.subList(0, 1));
-            numbers.removeAll(bValue);
-        }
-
-        ArrayList<Integer> spaceValues = new ArrayList<>(whitelist);
-
-        if (spaceValues.size() < 19){
-            numbers.removeAll(spaceValues);
-            int currentSize = spaceValues.size();
-            Collections.shuffle(numbers);
-            spaceValues.addAll(numbers.subList(0, currentSize));
-        }
-
-        if (whitelist.size() > 19){
-            Collections.shuffle(spaceValues);
-            ArrayList<Integer> spaceTemp = new ArrayList<>(spaceValues.subList(0, 19));
-            spaceValues = spaceTemp;
-        }
-
-        numbers.removeAll(spaceValues);
-
-        putativeKey.put("space", spaceValues);
-
-        if (bValue.size() == 0){
-            Collections.shuffle(numbers);
-            ArrayList<Integer> bTemp = new ArrayList<>(numbers.subList(0, 1));
-            numbers.removeAll(bTemp);
-            bValue.addAll(bTemp);
-        }
-
-        ArrayList<Integer> bNum = new ArrayList<>(bValue);
-
-        putativeKey.put("b", bNum);
-
-        blacklist.addAll(numbers);
-
-        ArrayList<Integer> leftNum = new ArrayList<>(blacklist);
-        Collections.shuffle(leftNum);
-
-        int partition = 0;
-
-        for(String key: map.keySet()) {
-            if (key.equals("space")){
-                partition = partition + map.get(key);
-            }
-            else if (key.equals("b")){
-                partition = partition + map.get(key);
-            }
-            else {
-                putativeKey.put(key, new ArrayList<>(leftNum.subList(partition, partition + map.get(key))));
-                partition = partition + map.get(key);
+                if (space == 19)
+                    break;
             }
         }
+
+        for (int i = 0; i < ciphertext.length; i++){
+            System.out.print(ciphertext[i]+" ");
+        }
+        System.out.println("");
+        System.out.println(spaceValues);
+
+        System.out.println(whitelist);
+
+        System.out.println(blacklist);
+
+//        if (bValue.size() > 1){
+//            ArrayList<Integer> bTemp = new ArrayList<>(bValue);
+//            Collections.shuffle(bTemp);
+//            bValue.clear();
+//            bValue.addAll(bTemp.subList(0, 1));
+//            blacklist.removeAll(bValue);
+//        }
+//
+//        if (bValue.size() == 0){
+//            Collections.shuffle(numbers);
+//            ArrayList<Integer> bTemp = new ArrayList<>(numbers.subList(0, 1));
+//            blacklist.removeAll(bTemp);
+//            bValue.addAll(bTemp);
+//        }
+
+//
+//            else if (whitelist.contains(ciphertext[i]) && !bValue.contains(ciphertext[i])){
+//                if (i < ciphertext.length - 1) {
+//                    blacklist.add(ciphertext[i + 1]);
+//                    whitelist.remove(ciphertext[i + 1]);
+//                }
+//                if (i < ciphertext.length - 2) {
+//                    blacklist.add(ciphertext[i + 2]);
+//                    whitelist.remove(ciphertext[i + 2]);
+//                }
+//                if (i < ciphertext.length - 3) {
+//                    blacklist.add(ciphertext[i + 3]);
+//                    whitelist.remove(ciphertext[i + 3]);
+//                }
+//            }
+//            else if (!blacklist.contains(ciphertext[i]) && !bValue.contains(ciphertext[i]))
+//                whitelist.add(ciphertext[i]);
+//        }
+//
+//        if (bValue.size() > 1){
+//            ArrayList<Integer> bTemp = new ArrayList<>(bValue);
+//            Collections.shuffle(bTemp);
+//            bValue.clear();
+//            bValue.addAll(bTemp.subList(0, 1));
+//            blacklist.removeAll(bValue);
+//        }
+//
+//        if (bValue.size() == 0){
+//            Collections.shuffle(numbers);
+//            ArrayList<Integer> bTemp = new ArrayList<>(numbers.subList(0, 1));
+//            blacklist.removeAll(bTemp);
+//            bValue.addAll(bTemp);
+//        }
+//
+//        int limit = keyspace - 20;
+//
+//        while (blacklist.size() < limit){
+//            Collections.shuffle(numbers);
+//            blacklist.addAll(numbers.subList(0, 1));
+//        }
+//
+//        ArrayList<Integer> spaceValues = new ArrayList<>(whitelist);
+//        ArrayList<Integer> otherChars = new ArrayList<>(blacklist);
+//
+//        if (spaceValues.size() < 19){
+//            int currentSize = spaceValues.size();
+//            Collections.shuffle(otherChars);
+//            spaceValues.addAll(otherChars.subList(0, currentSize));
+//            otherChars.removeAll(otherChars.subList(0, currentSize));
+//        }
+//
+//        if (whitelist.size() > 19){
+//            Collections.shuffle(spaceValues);
+//            ArrayList<Integer> spaceTemp = new ArrayList<>(spaceValues.subList(0, 19));
+//            spaceValues = spaceTemp;
+//            otherChars.removeAll(spaceValues.subList(19, spaceValues.size()));
+//        }
+//
+//        putativeKey.put("space", spaceValues);
+//
+//        ArrayList<Integer> bNum = new ArrayList<>(bValue);
+//
+//        putativeKey.put("b", bNum);
+//
+//        blacklist.addAll(numbers);
+//
+//        ArrayList<Integer> leftNum = new ArrayList<>(blacklist);
+//        Collections.shuffle(leftNum);
+//
+//        int partition = 0;
+//
+//        for(String key: map.keySet()) {
+//            if (key.equals("space")){
+//                partition = partition + map.get(key);
+//            }
+//            else if (key.equals("b")){
+//                partition = partition + map.get(key);
+//            }
+//            else {
+//                putativeKey.put(key, new ArrayList<>(leftNum.subList(partition, partition + map.get(key))));
+//                partition = partition + map.get(key);
+//            }
+//        }
         return putativeKey;
     }
 
