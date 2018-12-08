d8a881a928348399408eefe470b2dd648809e86c
Francois Van Laer <fvl209@nyu.edu>
2018-12-08 16:40:48 -0500

    Added other assertions + comments + simulation of 10 CPAs

--- src/test/java/org/nyu/crypto/ChosenPlaintextAttack.java
+++ src/test/java/org/nyu/crypto/ChosenPlaintextAttack.java
@@ -9,6 +9,7 @@ import org.nyu.crypto.service.MessageGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -32,6 +33,9 @@ public class ChosenPlaintextAttack {
     @Autowired
     private Decryptor decryptor;
 
+    @Value("${charset.length}")
+    private int charsetLength;
+
     private Logger logger = LoggerFactory.getLogger(ChosenPlaintextAttack.class);
 
     @Test
@@ -43,12 +47,15 @@ public class ChosenPlaintextAttack {
         int totalValues = 0;
         int round = 0;
 
-        while (totalValues < 106) {
+        // No 'q' in dictionary ->> therefore 106 - 1 = 105
+        while (totalValues < 105) {
 
+            totalValues = 0;
             String plaintext = messageGenerator.generateMessage();
             int[] ciphertext = encryptor.encrypt(actualMap, plaintext);
 
             for (int i = 0; i < ciphertext.length; i++) {
+
                 String currentLetter = Character.toString(plaintext.charAt(i));
                 int currentNum = ciphertext[i];
 
@@ -66,7 +73,7 @@ public class ChosenPlaintextAttack {
                     }
                 }
 
-                // Other letters
+                // Other characters
                 else {
                     if (newMap.containsKey(currentLetter)) {
                         HashSet<Integer> temp = new HashSet<>(newMap.get(currentLetter));
@@ -92,6 +99,27 @@ public class ChosenPlaintextAttack {
         int[] ciphertext = encryptor.encrypt(actualMap, plaintext);
         String result = decryptor.decrypt(newMap, ciphertext);
 
+        // Asserting whether the resulting plaintext using the new key is equal to the original plaintext
+
+        logger.info("Original plaintext: " + plaintext);
+        logger.info("Resulting plaintext: " + result);
         assert (result.equals(plaintext));
+
+        // Asserting whether the number of keys in the new keymap is equal to 27
+
+        //assert (newMap.size() == charsetLength);
+
+        // Asserting whether the keyspace for each character is correct
+        for (String key: newMap.keySet()){
+            assert (newMap.get(key).size() == actualMap.get(key).size());
+        }
+    }
+
+    @Test
+    public void CPASimulation(){
+
+        for (int i = 0; i < 10; i++){
+            ChosenPlaintextAttackGen();
+        }
     }
 }
